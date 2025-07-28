238b39708dda54248bf5132b2690de2cbf710552
ztdeb14134 <ztdeb14134>
2025-07-28 09:40:42 +0800

    code rebuild

--- src/China_sports_lottery.rs
+++ src/China_sports_lottery.rs
@@ -1,13 +1,20 @@
 use colored::Colorize;
 use rand::{self, Rng, seq::SliceRandom};
 use std::{
-    io::{self, Write, stdout},
+    io::{Write, stdout},
     sync::{
         Arc,
         atomic::{AtomicUsize, Ordering},
     },
     thread,
 };
+#[derive(Debug)]
+pub enum PlayType {
+    Single,
+    Duplex(usize, usize),
+    KeyFiller(usize, usize, usize, usize),
+}
+#[derive(Debug)]
 pub struct SuperLotto {
     /*
      *5+2                   1/21,425,712
@@ -20,32 +27,33 @@ pub struct SuperLotto {
      *3+1 2+2               127,600/21,425,712
      *3+0 2+1 1+2 0+2       1,777,661/21,425,712
      */
-    pub front: usize,
-    pub behind: usize,
+    pub multiple: Option<usize>,
+    pub play_type: PlayType,
     pub number: usize,
+    pub Additional: bool,
 }
 impl SuperLotto {
-    pub fn new(_front: usize, _behind: usize, _number: usize) -> Self {
+    pub fn new(play_type: PlayType, number: usize, Additional: bool) -> Self {
         SuperLotto {
-            front: _front,
-            behind: _behind,
-            number: _number,
+            multiple: None,
+            play_type,
+            number,
+            Additional,
         }
     }
-
-    pub fn run(&self) -> Result<(), ()> {
-        let mut result: Vec<Vec<String>> = Vec::new();
+    pub fn set_multiple(&mut self, multiple: usize) {
+        self.multiple = Some(multiple);
+    }
+    pub fn draw(&mut self) -> Result<Vec<String>, ()> {
+        let mut result: Vec<String> = Vec::new();
         for x in 0..self.number {
             let progress = Arc::new(AtomicUsize::new(0));
             let progress_clone = Arc::clone(&progress);
-            println!("\n开始第{}次roll号码", x + 1);
             let mut first: Vec<String> = (1..=35).map(|num| format!("{:02}", num)).collect();
             let mut last: Vec<String> = (1..=12).map(|num| format!("{:02}", num)).collect();
             let mut rng: rand::prelude::ThreadRng = rand::rng();
-
             let times: usize = rng.random_range(19770801..20130330);
-
-            let handle = thread::spawn(move || {
+            let handle = Some(thread::spawn(move || {
                 loop {
                     let val = progress_clone.load(Ordering::Relaxed);
                     if val == times {
@@ -61,34 +69,109 @@ impl SuperLotto {
                         .to_string();
                     let space = " ".repeat(empty);
                     print!(
-                        "\r{}{} {:>3}",
+                        "\r第{}次号码：{}{} {:>3}",
+                        x + 1,
                         bar,
                         space,
                         (percent * 100.0).round() as usize
                     );
                     stdout().flush().unwrap();
                 }
-            });
-            for _ in 0..times {
-                first.shuffle(&mut rng);
-                last.shuffle(&mut rng);
-                progress.fetch_add(1, Ordering::Relaxed);
+            }));
+            match self.play_type {
+                PlayType::Single => {
+                    for _ in 0..times {
+                        first.shuffle(&mut rng);
+                        last.shuffle(&mut rng);
+                        progress.fetch_add(1, Ordering::Relaxed);
+                    }
+                    let mut front_result: Vec<String> = first.iter().take(5).cloned().collect();
+                    let mut behind_result: Vec<String> = last.iter().take(2).cloned().collect();
+                    front_result.sort();
+                    behind_result.sort();
+                    result.push(format!(
+                        "第{}注: {} : {}{}",
+                        x + 1,
+                        front_result.join(" "),
+                        behind_result.join(" "),
+                        format!(
+                            "{}{}",
+                            if self.multiple.is_some() {
+                                format!("{}倍", self.multiple.unwrap())
+                            } else {
+                                "".to_string()
+                            },
+                            if self.Additional { "追加" } else { "" }
+                        )
+                    ));
+                }
+                PlayType::Duplex(front, behind) => {
+                    for _ in 0..times {
+                        first.shuffle(&mut rng);
+                        last.shuffle(&mut rng);
+                        progress.fetch_add(1, Ordering::Relaxed);
+                    }
+                    let mut front_result: Vec<String> = first.iter().take(front).cloned().collect();
+                    let mut behind_result: Vec<String> =
+                        last.iter().take(behind).cloned().collect();
+                    front_result.sort();
+                    behind_result.sort();
+                    result.push(format!(
+                        "第{}注: {} : {}{}",
+                        x + 1,
+                        front_result.join(" "),
+                        behind_result.join(" "),
+                        format!(
+                            "{}{}",
+                            if self.multiple.is_some() {
+                                format!("{}倍", self.multiple.unwrap())
+                            } else {
+                                "".to_string()
+                            },
+                            if self.Additional { "追加" } else { "" }
+                        )
+                    ));
+                }
+                PlayType::KeyFiller(key_front, filler_front, key_behind, filler_behind) => {
+                    for _ in 0..times {
+                        first.shuffle(&mut rng);
+                        last.shuffle(&mut rng);
+                        progress.fetch_add(1, Ordering::Relaxed);
+                    }
+                    let mut keyfront: Vec<String> = first.drain(0..key_front).collect();
+                    let mut keybehind_result: Vec<String> = last.drain(0..key_behind).collect();
+                    keyfront.sort();
+                    keybehind_result.sort();
+                    let mut filler_front_result: Vec<String> =
+                        first.drain(0..filler_front).collect();
+                    let mut filler_behind_result: Vec<String> =
+                        last.drain(0..filler_behind).collect();
+                    filler_front_result.sort();
+                    filler_behind_result.sort();
+
+                    result.push(format!(
+                        "第{}注{}:\n前区胆:{}\n前区拖:{}\n后区胆:{}\n后区拖:{}",
+                        x + 1,
+                        format!(
+                            "{}{}",
+                            if self.multiple.is_some() {
+                                format!("{}倍", self.multiple.unwrap())
+                            } else {
+                                "".to_string()
+                            },
+                            if self.Additional { "追加" } else { "" }
+                        ),
+                        keyfront.join(" "),
+                        filler_front_result.join(" "),
+                        keybehind_result.join(" "),
+                        filler_behind_result.join(" ")
+                    ));
+                }
             }
-            let mut this_result: Vec<String> = first.iter().take(self.front).cloned().collect();
-            let mut c: Vec<String> = last.iter().take(self.behind).cloned().collect();
-
-            this_result.sort();
-            c.sort();
-
-            this_result.push(":".to_string());
-            this_result.append(&mut c);
-            result.push(this_result);
-            handle.join().unwrap();
+            if let Some(handle) = handle {
+                handle.join().unwrap();
+            }
         }
-        println!();
-        result
-            .iter()
-            .for_each(|x| x.iter().for_each(|XX| println!("{} ", XX)));
-        Ok(())
+        Ok(result)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,53 @@
 #[allow(non_snake_case)]
-mod China_Welfare_Lottery;
-#[allow(non_snake_case)]
 mod China_sports_lottery;
-use crate::{
-    China_Welfare_Lottery::{HappyEight, RBball, SevenLotto},
-    China_sports_lottery::*,
-};
+use crate::China_sports_lottery::*;
 use chrono::prelude::*;
 fn main() {
     match Local::now().date_naive().weekday() {
         chrono::Weekday::Mon => {
-            let super_lotto = SuperLotto::new(5, 4, 1);
-            super_lotto.run().unwrap();
+            let mut super_lotto = SuperLotto::new(PlayType::KeyFiller(5, 0, 1, 11), 1, false);
+            let _ = super_lotto
+                .draw()
+                .unwrap()
+                .into_iter()
+                .map(|s| println!("{}", s));
             println!("12 13 19 25 34 : 05 06\n07 09 13 19 27 : 04 08");
-            println!("+18大乐透套餐");
         }
         chrono::Weekday::Tue => {
-            let super_lotto = SuperLotto::new(5, 3, 2);
-            super_lotto.run().unwrap();
+            let mut super_lotto = SuperLotto::new(PlayType::Duplex(5, 3), 2, false);
+            let _ = super_lotto
+                .draw()
+                .unwrap()
+                .into_iter()
+                .map(|s| println!("{}", s));
             println!("12 13 19 25 34 : 05 06\n07 09 13 19 27 : 04 08");
         }
         chrono::Weekday::Wed => {
-            let super_lotto = SuperLotto::new(5, 2, 2);
-            super_lotto.run().unwrap();
-            println!("全部追加")
+            let mut super_lotto = SuperLotto::new(PlayType::Single, 2, true);
+            super_lotto.set_multiple(3);
+            let _ = super_lotto
+                .draw()
+                .unwrap()
+                .into_iter()
+                .map(|s| println!("{}", s));
         }
         chrono::Weekday::Thu => {
             println!("12 13 19 25 34 : 05 06\n07 09 13 19 27 : 04 08");
-            let super_lotto = SuperLotto::new(6, 2, 1);
-            super_lotto.run().unwrap();
+            let mut super_lotto = SuperLotto::new(PlayType::Duplex(6, 2), 1, false);
+            let _ = super_lotto
+                .draw()
+                .unwrap()
+                .into_iter()
+                .map(|s| println!("{}", s));
         }
         chrono::Weekday::Fri => {
-            println!("胆码");
-            let super_lotto = SuperLotto::new(2, 1, 1);
-            super_lotto.run().unwrap();
-            println!("托码");
-            let super_lotto = SuperLotto::new(5, 2, 1);
-            println!("托码单独打一注追加");
-            super_lotto.run().unwrap();
+            let mut super_lotto = SuperLotto::new(PlayType::KeyFiller(2, 5, 1, 2), 1, false);
+            let _ = super_lotto
+                .draw()
+                .unwrap()
+                .into_iter()
+                .map(|s| println!("{}", s));
         }
-        chrono::Weekday::Sat => println!("节制"),
-        chrono::Weekday::Sun => println!("节制"),
-    }
-
-    if false {
-        let happy_eight = HappyEight::new(10, 1);
-        let rbball = RBball::new(6, 1, 1);
-        let sevenlotto = SevenLotto::new(7, 1);
-        happy_eight.run().unwrap();
-        rbball.run().unwrap();
-        sevenlotto.run().unwrap();
+        _ => println!("节制"),
     }
 }
